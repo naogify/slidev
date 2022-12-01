2542e9af684ddd8ba5d697a0e54f15e6cc25b783
naoki <n.globe.us@gmail.com>
2022-12-02 08:45:30 +0900

    initial commit

---
+++ components/Map.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const { geolonia } = window;
+  const mapDom = document.getElementById('map')
+  if (mapDom) {
+    const map = new geolonia.Map('#map')
+    map.on('load', () => {
+      console.log("map loaded")
+    })
+  }
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
